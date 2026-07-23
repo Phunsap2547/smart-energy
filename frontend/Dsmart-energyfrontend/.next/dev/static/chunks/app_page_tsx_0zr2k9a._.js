818907cfff2416b486f0a57fda523d9fdb9dc903
phunsap47 <phunsap@example.com>
2026-07-23 16:42:26 +0700

    new

---
+++ frontend/Dsmart-energyfrontend/.next/dev/static/chunks/app_page_tsx_0zr2k9a._.js
@@ -0,0 +1,5 @@
+(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: ["static/chunks/node_modules_recharts_es6_util_0faq6ho._.js","static/chunks/node_modules_recharts_es6_state_0nzxah_._.js","static/chunks/node_modules_recharts_es6_cartesian_0umlkln._.js","static/chunks/node_modules_recharts_es6_0802nqc._.js","static/chunks/node_modules_0pxwy_1._.js","static/chunks/app_01257-a._.js"],
+    source: "dynamic"
+});
